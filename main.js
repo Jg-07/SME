7ab86d39f62200eb2db5fa281c83a68eedb7765e
jaias <jaiastrobaskar@gmail.com>
2022-04-02 19:44:47 +0530

    commit
    Signed-off-by: jaias <jaiastrobaskar@gmail.com>

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import './style.css'
+import Typed from 'typed.js'
 
+var typed = new Typed('.typed', {
+    strings: [
+        "We Ascend",
+        "We Conquer",
+        "Welcome to the hub of royal mechons",
+        "The Society of Mechanical engineering,CEG"],
+    typeSpeed: 60,
+    backSpeed: 30,
+    backDelay: 3300,
+    loop: true,
+});
